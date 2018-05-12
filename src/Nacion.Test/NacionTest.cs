d337edec67b248c5a9c79cd78f28ea447cb67256
Mario Moreno <mamcer@outlook.com>
2018-05-12 15:13:46 -0300

    Nacion.Test refactor

--- src/Nacion.Test/NacionTest.cs
+++ src/Nacion.Test/NacionTest.cs
@@ -13,47 +13,11 @@ namespace Nacion.Test
     [TestClass]
     public class NacionTest
     {
-        private TestContext testContextInstance;
-
         /// <summary>
         ///Gets or sets the test context which provides
         ///information about and functionality for the current test run.
         ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
-        #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        //[ClassInitialize()]
-        //public static void MyClassInitialize(TestContext testContext) 
-        //{
-        //}
-        
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
+        public TestContext TestContext { get; set; }
 
         [TestMethod]
         public void TestBasicoCredito()
@@ -104,7 +68,7 @@ namespace Nacion.Test
             int total = Credito.Instancia.GetCantidadCuotasNuevas();
             Assert.AreEqual(total, 208, "Cantidad de cuotas nuevas inválido.");
         }
-        
+
         [TestMethod]
         public void TestPrimerVencimiento()
         {
@@ -137,15 +101,15 @@ namespace Nacion.Test
         public void TestPagarCuota()
         {
             Cuota siguienteCuota = Credito.Instancia.GetSiguienteCuota();
-            
+
             Credito.Instancia.PagarCuotaNro(siguienteCuota.Nro + 10);
             Cuota cuota = Credito.Instancia.GetCuotaNro(siguienteCuota.Nro + 10);
             Assert.AreEqual(cuota.Status, StatusCuota.Nueva, "Solo la siguiente cuota disponible podría pagarse.");
-            
+
             Credito.Instancia.PagarCuotaNro(siguienteCuota.Nro);
             cuota = Credito.Instancia.GetCuotaNro(siguienteCuota.Nro);
             Assert.AreEqual(cuota.Status, StatusCuota.Pagada, "La siguiente cuota disponible debería estar paga ahora.");
-            
+
             Credito.Instancia.AdelantarCuotaNro(siguienteCuota.Nro);
             cuota = Credito.Instancia.GetCuotaNro(siguienteCuota.Nro);
             Assert.AreNotEqual(cuota.Status, StatusCuota.Adelantada, "No se puede adelantar una cuota paga.");
